6f4084cb4bafe2c2cf3a50b207fcd3b6c2a6a36f
Evandro Calado <evandrocalado07@gmail.com>
2024-02-19 20:57:10 -0300

    ✨ feat(server): create jobs endpoint

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import jobRouter from "./routes/jobRouter.js";
 
-const app = express();
 dotenv.config();
+const app = express();
 
+app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
-
+// index
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Wellcome to jobify API!" });
+});
+
+app.use("/api/v1/jobs", jobRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "Something went wrong" });
 });
 
 const port = process.env.PORT || 5100;
