f09887c897603c900f77493a826662f704f67043
Evandro Calado <evandrocalado07@gmail.com>
2024-02-21 20:11:56 -0300

    🐳 chore(controllers): hash password
    add hash password and first user create as admin on application

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const isFirstAccount = (await User.countDocuments()) === 0;
 
-  res.status(StatusCodes.CREATED).json({ user });
+  req.body.role = isFirstAccount ? "admin" : "user";
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
+  await User.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
