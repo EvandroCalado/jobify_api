7457576f85a6ad8ccfa9bd1ef046d21084899bae
Evandro Calado <evandrocalado07@gmail.com>
2024-02-20 22:58:03 -0300

    🐞 fix(middleware): messages

--- middlewares/errorHandlerMiddleware.js
+++ middlewares/errorHandlerMiddleware.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  const msg = err.message || "Something went wrong, try again later";
+  const msg = err.message || "something went wrong, try again later";
 
   res.status(statusCode).json({ msg });
 };
