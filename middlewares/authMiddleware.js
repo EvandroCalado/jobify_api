6b11e2e49d05c2ccb5b86a3fdf342653f3c5bb3f
Evandro Calado <evandrocalado07@gmail.com>
2024-02-21 23:48:30 -0300

    ✨ feat(middlewares): authenticate user

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import { UnauthenticatedError } from "../errors/customErrors.js";
+import { verifyJwt } from "../utils/token.js";
+
+const authenticateUser = async (req, res, next) => {
+  const { jobifyToken } = req.cookies;
+
+  if (!jobifyToken) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+
+  try {
+    const { userId, role } = verifyJwt(jobifyToken);
+
+    req.user = { userId, role };
+
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+};
+
+export default authenticateUser;
