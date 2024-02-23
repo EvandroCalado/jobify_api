c6637f1392121face030eb393f8c0905eca039fc
Evandro Calado <evandrocalado07@gmail.com>
2024-02-23 09:03:17 -0300

    🐳 chore(routes): authorize permission

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,12 +4,16 @@ import {
   getCurrentUser,
   updateUser,
 } from "../controllers/userController.js";
+import { authorizePermissions } from "../middlewares/authMiddleware.js";
 import { validateUpdateUserInput } from "../middlewares/validationMiddleware.js";
 
 const router = Router();
 
 router.get("/current-user", getCurrentUser);
-router.get("/admin/app-stats", getApplicationStats);
+router.get("/admin/app-stats", [
+  authorizePermissions("admin"),
+  getApplicationStats,
+]);
 router.patch("/update-user", validateUpdateUserInput, updateUser);
 
 export default router;
